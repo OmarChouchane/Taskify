0c8a9f1cbd40613d2529f540af1a58c1962b5da8
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2026-01-28 22:39:43 +0100

    Integrate auth and user modules into app
    - Import UserModule and AuthModule
    - Register User entity in TypeORM
    - Add AuthGuard as global provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { User } from './user/entities/user.entity';
+import { AuthGuard } from './auth/auth/auth.guard';
 
 @Module({
   imports: [
+    UserModule,
+    AuthModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'kanban.db',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
-    // TODO: Import modules here
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthGuard],
 })
 export class AppModule {}
